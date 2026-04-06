9190fe4c5b7b0938a10bf4d9392aacbe821afbef
kashishpanchal2005 <2303031080214@paruluniversity.ac.in>
2026-04-06 16:55:40 +0530

    Create project.cs

---
+++ project.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator");
+
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter operator (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+
+        if (op == "+")
+            result = num1 + num2;
+        else if (op == "-")
+            result = num1 - num2;
+        else if (op == "*")
+            result = num1 * num2;
+        else if (op == "/")
+            result = num1 / num2;
+
+        Console.WriteLine("Result: " + result);
+    }
+}
